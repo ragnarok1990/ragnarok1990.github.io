4c5a935e086133ce83fff0191e15133137b851ac
ragnarok1990 <148160683+ragnarok1990@users.noreply.github.com>
2023-11-12 00:24:44 -0600

    Add files via upload

---
+++ ragnarok90_tiktok/docs/script.js
@@ -0,0 +1,10 @@
+ // Autoplay
+            $('#box4').flipbox({
+                vertical: false,
+                autoplay: true,
+                autoplayReverse: false,
+                autoplayWaitDuration: 2000,
+                autoplayPauseOnHover: false,
+               animationEasing: 'linear',
+               animationDuration: 600,
+            });
